ada7d00e191a2a405ca39d8ad5a2492e0401b567
tornike <tornike.skhulukhia.1@iliauni.edu.ge>
2022-10-20 13:08:12 +0400

    reformat code

--- person/extract.py
+++ person/extract.py
@@ -4,14 +4,17 @@ from . import NAMES, SURNAMES
 from .util import normalize_surname
 
 CHARACTERS_THAT_CAN_BE_USED_TO_JOIN_MULTI_NAME_HAVING_NAME_SURNAMES = [
-    "-", "—", "(", ")"   # ( ) ex: :ირაკლი (დაჩი) ბერაია
+    "-",
+    "—",
+    "(",
+    ")",  # ( ) ex: :ირაკლი (დაჩი) ბერაია
 ]
+
 
 def _normalize_text(text):
 
     # benchmark/test performance later using this and new text creation and other approaches...
 
-    
     for i in CHARACTERS_THAT_CAN_BE_USED_TO_JOIN_MULTI_NAME_HAVING_NAME_SURNAMES:
         # these symbols are concatinating possible one name-surnames ex: ლაშა-გიორგი გიორგაძე
         # so we should replace them with space
@@ -21,13 +24,21 @@ def _normalize_text(text):
     for i in string.punctuation:
         if i not in CHARACTERS_THAT_CAN_BE_USED_TO_JOIN_MULTI_NAME_HAVING_NAME_SURNAMES:
             # but all others should remain, but separated from words so that
-            # we can still identify after space-splitting if word is name/surname or not 
+            # we can still identify after space-splitting if word is name/surname or not
             text = text.replace(i, f" {i} ")
 
     # replace multiple spaces with just 1
     text = " ".join(text.split())
 
     return text
+
+
+def word_seems_name(word):
+    return word in NAMES
+
+
+def word_seems_surname(word):
+    return word in SURNAMES
 
 
 def extract_persons(text):
@@ -63,12 +74,16 @@ def extract_persons(text):
         next_word = splitted_words[i + 1]
 
         if i < len(splitted_words) - 3 and all(
-            [curr_word in NAMES, next_word in NAMES, splitted_words[i + 2] in NAMES]
+            [
+                word_seems_name(curr_word),
+                word_seems_name(next_word),
+                word_seems_name(splitted_words[i + 2]),
+            ]
         ):
 
             normalized_possible_surname = normalize_surname(splitted_words[i + 3])
 
-            if normalized_possible_surname in SURNAMES:
+            if word_seems_surname(normalized_possible_surname):
 
                 result.add(
                     f"{curr_word} {next_word} {splitted_words[i + 2]} {normalized_possible_surname}"
@@ -77,20 +92,24 @@ def extract_persons(text):
                 continue
 
         # 2 word-having name & 1 word-having surname test | ex: name = "სანდრა ელისაბედ", surname="რულოვსი"
-        if i < len(splitted_words) - 2 and curr_word in NAMES and next_word in NAMES:
+        if (
+            i < len(splitted_words) - 2
+            and word_seems_name(curr_word)
+            and word_seems_name(next_word)
+        ):
             normalized_possible_surname = normalize_surname(splitted_words[i + 2])
 
-            if normalized_possible_surname in SURNAMES:
+            if word_seems_surname(normalized_possible_surname):
                 result.add(f"{curr_word} {next_word} {normalized_possible_surname}")
                 skip_indexes.update([i, i + 1, i + 2])
                 continue
 
         # 1 word-having name & 1 word-having surname test | ex: "გიორგი გიორგობიანი"
-        if curr_word in NAMES:
+        if word_seems_name(curr_word):
             # maybe next word is surname
             normalized_possible_surname = normalize_surname(next_word)
 
-            if normalized_possible_surname in SURNAMES:
+            if word_seems_surname(normalized_possible_surname):
                 result.add(f"{curr_word} {normalized_possible_surname}")
                 skip_indexes.update([i, i + 1])
 
